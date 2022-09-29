cca5730e565d10868f7fc0bf603aa5d5b7f8ca90
Mikalach <mika.djetic@gmaiul.com>
2022-09-29 10:41:49 -0700

    tentative task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *
- *return : allways 0 (succeess)
- */
+*main - entry point
+*
+*return : allways 0 (succeess)
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
