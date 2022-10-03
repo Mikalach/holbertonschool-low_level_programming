615b6504f6ed2dfee161add125d4315317a5029c
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 11:11:00 -0700

    not the first commit at all

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+include <stdio.h>
 /**
  * main- count to 9 with comas and space
  * @void: Null value
@@ -11,13 +10,13 @@ int main(void)
 {
 int n;
  
-for (n = '0'; n <= '9'; n++)
-putchar(n);
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
 
-if (n != '9')
-putchar(',');
-putchar(' ');
-else
+		if (n != '9')
+		putchar(',');
+		putchar(' ');
+		else
 putchar('\n');
 return (0);
 }
