77bbc92e28d87e2f5757e7e2a564de7f90171684
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 02:08:40 -0700

    betty check comment

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Check if a number.
+ * main - check if a number.
  * @void: Null value
  *
  * Return: Zero value
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-  int n;
+int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
