5f5cfd775839ac2d2c04ae1e9e9db0844dbe9e8a
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 06:20:53 -0700

    commit task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main- Entry point
+* @void: Null value
+*
+* Return: Zero value
+*/
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
