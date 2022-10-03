8ba01e775da92ebe5b3e1b61a0c47eb5bef8e6cf
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 11:28:11 -0700

    Forgot the curly brackets

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-  int n;
+int n;
 
-  for (n = '0'; n <= '9'; n++)
-    {
-      putchar(n);
-      if (n != '9')
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+if (n != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
