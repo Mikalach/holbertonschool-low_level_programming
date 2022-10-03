171288235eaac12d64fb25a3e33493f77dced1cc
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 11:02:53 -0700

    else without if statement?

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -14,11 +14,10 @@ int n;
 for (n = '0'; n <= '9'; n++)
 putchar(n);
 
- if (n != '9')
-   putchar(',');
-   putchar(' ');
-
- else
-   putchar('\n');
+if (n != '9')
+putchar(',');
+putchar(' ');
+else
+putchar('\n');
 return (0);
 }
