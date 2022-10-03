43c16b61f7ba290b7585041d36c75834eb298a57
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 10:44:38 -0700

    7th task reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- reverse alphabet
+ * @void: Null value
+ *
+ * Return: Zero value
+ */
+
+int main(void)
+{
+char n;
+
+for (n = 'z'; n <= 'a'; n--)
+putchar(n);
+putchar('\n');
+return (0);
+}
