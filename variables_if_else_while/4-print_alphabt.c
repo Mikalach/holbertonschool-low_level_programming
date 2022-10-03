1f86639ad7dd27e06ea4123a1f037c923355092a
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 06:59:36 -0700

    lol wrong file name

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Return: Zero value
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == 'q' || c == 'e')
+continue;
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
