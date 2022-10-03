34eec03efcbb410e94a03308f2eca88df7617bd5
Mikalach <mika.djetic@gmaiul.com>
2022-10-03 10:59:03 -0700

    it should be good first try

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- count to 9 with comas and space
+ * @void: Null value
+ *
+ * Return: Zero value
+ */
+
+int main(void)
+{
+int n;
+ 
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+
+if (n != '9')
+putchar(',');
+putchar(' ');
+
+else 
+putchar('\n');
+return (0);
+}
