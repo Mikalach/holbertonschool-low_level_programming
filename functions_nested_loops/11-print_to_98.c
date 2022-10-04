63ae15da62e83afd55665d4d8841ef15da51f732
Mikalach <mika.djetic@gmaiul.com>
2022-10-04 14:24:03 -0700

    removed the return 0

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,22 +4,15 @@
  * print_to_98 - prints all natural numbers between n and 98
  *
  * @n: the given natural number
- *
- * Return: 0 = success
- *
  */
 void print_to_98(int n)
 {
+	if (n <= 98)
 	{
-		if (n <= 98)
-		{
-			for (n = n; n <= 97; n++)
-				printf("%d, ", n);
-		}
-		else
-			for (n = n; n > 98; n--)
-				printf("%d, ", n);
-		printf("98\n");
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	}
-	return (0);
+	else
+		for (n = n; n >= 98; n--)
+			printf("%d, ", n);
 }
