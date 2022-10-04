0f8259c81e639f8a5422e5f7d99716e785ca23d7
Mikalach <mika.djetic@gmaiul.com>
2022-10-04 10:21:23 -0700

    Should be an easy task

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+\**
+*_abs - find and return the absolute value of a given integer
+*
+*@n: the given integer
+*
+*Return the absolute value of the given integer
+*
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (-n);
+	}
+}
