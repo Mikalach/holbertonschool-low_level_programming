8ebcd6b5e577706862f3e71ddef134823a2c17e9
Mikalach <mika.djetic@gmaiul.com>
2022-10-04 14:11:34 -0700

    abc as variable and returnc

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add numbers a and b
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Sum to two numbers
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
