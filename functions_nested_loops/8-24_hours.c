b80e11d18cc737fe8eec700e300c198d4da34e9a
Mikalach <mika.djetic@gmaiul.com>
2022-10-04 12:05:24 -0700

    24 hours chrono v1.4

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,11 +7,14 @@
 void jack_bauer(void)
 {
 	int h, m;
+
 	h = 0;
+
 	while (h < 24)
 	{
 		m = 0;
 		while (m < 60)
+		{
 			_putchar((h / 10) + '0');
 		        _putchar((h % 10) + '0');
 			_putchar(':');
@@ -19,6 +22,7 @@ void jack_bauer(void)
 			_putchar((m % 10) + '0');
 			_putchar('\n');
 			m++;
+		}
 	}
 	h++;
 }
