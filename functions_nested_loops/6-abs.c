c44c7446a02adb9fa1fd98198aff055f6ea11d08
Mikalach <mika.djetic@gmaiul.com>
2022-10-04 10:22:57 -0700

    Should be an easy task 2nd try

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
-\**
-*_abs - find and return the absolute value of a given integer
-*
-*@n: the given integer
-*
-*Return the absolute value of the given integer
-*
-*/
+ /**
+ *_abs - find and return the absolute value of a given integer
+ *
+ *@n: the given integer
+ *
+ *Return the absolute value of the given integer
+ *
+ */
 
 int _abs(int n)
 {
