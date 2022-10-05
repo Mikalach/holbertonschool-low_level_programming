9fe718c51da98a8830e98ada7c2c71aafc308ef7
Mikalach <mika.djetic@gmaiul.com>
2022-10-05 15:22:56 -0700

    same as 3rd with if and continue at 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
