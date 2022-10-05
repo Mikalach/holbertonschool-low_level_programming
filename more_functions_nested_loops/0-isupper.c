e4e4efec4d79f373ad9fd066f0acdf804b96a8e4
Mikalach <mika.djetic@gmaiul.com>
2022-10-05 14:30:48 -0700

    task 0 check if upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,14 @@
-#include main.h
+#include "main.h"
+
+/**
+ * _isupper - Check if UPPERCASE
+ * @c: The character checked
+ * Return: 1 if c is UPPERCASE, else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
