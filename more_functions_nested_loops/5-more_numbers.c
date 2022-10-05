d6354e267db18cb8017fd6e0b8f2980f0cf89413
Mikalach <mika.djetic@gmaiul.com>
2022-10-05 15:49:58 -0700

    fixing 2 digit error

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,16 @@
 
 void more_numbers(void)
 {
-      	int m, n;
+	int m, n;
 	m = 0;
 
 	while (m < 10)
-
 	{
 		n = 0;
 		while (n < 15)
 		{
 			if (n > 9)
 			{
-
 				_putchar((n / 10) + '0');
 			}
 			_putchar((n % 10) + '0');
