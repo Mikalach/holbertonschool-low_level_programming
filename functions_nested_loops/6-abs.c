28b92c2d923ee0b869e03f247d45a09d191aca11
Mikalach <mika.djetic@gmaiul.com>
2022-10-05 06:25:14 -0700

    removed the space before comments

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
- /**
+/**
  * _abs - this should give the absolute value of a given integer
  *
  * @n: the given integer
  *
- * Return the absolute value of the given integer
+ * Return: the absolute value of the given integer
  *
  */
 
