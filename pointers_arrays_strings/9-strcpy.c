e0001afc0742c23ba4d447805d8bf1993a948863
Mikalach <mika.djetic@gmaiul.com>
2022-10-10 13:40:40 -0700

    testing options on the commit

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string inside an array pointed by src
+ * @dest: the array
+ * @src: the string to be copied
+ *
+ * Return: \0
+ */
+char *_strcpy(char *dest, char *src)
+
+{
+	int i = 0;
+
+	while (*src++)
+		i++;
+	len = i;
+	for (i = 0; i != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
