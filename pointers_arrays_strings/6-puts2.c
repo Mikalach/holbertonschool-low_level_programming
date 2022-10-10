4e8bb44d095d9e81d556902b09a40bd54a22cbc2
Mikalach <mika.djetic@gmaiul.com>
2022-10-10 12:26:24 -0700

    Trying a method with i + 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - function
+ * @str: string
+ * Return: 1
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i = i + 2)
+	{
+		if (i == 0)
+		{
+			_putchar(str[i]);
+		}
+		else if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
