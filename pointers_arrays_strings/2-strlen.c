585f23fdd9510696299a4d841734c6b32d8186ab
Mikalach <mika.djetic@gmaiul.com>
2022-10-10 04:16:35 -0700

    That count the number a char in a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - return the lenght of a given string
+ * @s: string
+ *
+ * Return: lenght of the given string
+ */
+
+int _strlen(char *s)
+
+{
+	a = 0;
+	while (*s =! '\0')
+	{
+		s++;
+		a++;
+	}
+	return(a);
+}
