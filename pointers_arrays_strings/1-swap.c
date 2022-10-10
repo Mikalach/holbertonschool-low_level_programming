f3292916994540e19f6349697e070608e7496f19
Mikalach <mika.djetic@gmaiul.com>
2022-10-10 01:47:32 -0700

    swap with a 3 variable

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swap the integers values of 2 variables
+ * @a: first value
+ * @b: second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int keep;
+	keep = *a;
+	*a = *b;
+	*b = keep;
+}
