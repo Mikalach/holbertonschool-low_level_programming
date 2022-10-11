1ed85be521a40ca104437085faf043d107a5042c
Mikalach <mika.djetic@gmaiul.com>
2022-10-11 11:53:04 -0700

    new way to do it with usual reverse

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
+	int i;
+	int tmp;
 
-	n = -1;
 	for (i = 0; i < n; i++)
 	{
+		n--;
 		tmp = a[i];
-		a[i] = a[n - i];
-		a[n - i] = tmp;
+		a[i] = a[n];
+		a[n] = tmp;
 	}
 }
