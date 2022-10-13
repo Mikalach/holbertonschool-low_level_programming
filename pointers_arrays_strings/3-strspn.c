f2dff3dc89b216882584c8393278316823b50dd8
Mikalach <mika.djetic@gmaiul.com>
2022-10-13 10:06:46 -0700

    Thanks Alexandre for substring explain

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- *
+ *_strspn - check the prefix of the string s with the accept char
+ *@s : the string to chekc
+ *@accept : the chars you want to count in the prefix
+ *Return: unsigned int that is the number of bytes in the prefix
  */
 
 unsigned int _strspn(char *s, char *accept)
