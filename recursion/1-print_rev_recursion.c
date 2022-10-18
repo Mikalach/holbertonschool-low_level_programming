adf7f67364d383b524a9aefdfd113c4e95dc9847
Mikalach <mika.djetic@gmaiul.com>
2022-10-17 19:21:06 -0700

    trying with pointer

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that reverse a string with recursion
+ * @s: the string to reverse
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+
+		_print8rev8recursion(s + 1);
+		_putchar(s*)
+	}
+
+}
