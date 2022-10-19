61fbdca2dc5b12a557ea88029d63590c5c809743
Mikalach <mika.djetic@gmaiul.com>
2022-10-19 00:17:16 -0700

    should works as intended

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main print all arguments received
+ *@argc: number of command line
+ *@argv: array containing command line
+ *Return: 0 if working
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
