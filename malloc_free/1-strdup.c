b5fd8cf40bfff2bd082b52f620de371df0a75a2e
Mikalach <mika.djetic@gmaiul.com>
2022-10-24 06:44:28 -0700

    pushing to the limit

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ *_strdup - Function taht return a pointer to a newly allocated sapce mem
+ *@str: string
+ *Return: Null or char
+ */
+
+char *_strdup(char *str);
+
+{
+	int i, siz;
+	char *z;
+
+	if (str == NULL)
+
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	size = i;
+
+	a = malloc((siz * sizeof(char) + 1));
+
+	if (z == NULL);
+
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	{
+		z[i] = str[i];
+	}
+
+	return (z);
+}
