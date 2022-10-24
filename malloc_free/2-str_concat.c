ec950505b155e2dd7dabc0c5f65ee6893e094972
Mikalach <mika.djetic@gmaiul.com>
2022-10-24 11:05:50 -0700

    trying to fix the betty coding style

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,64 +1,39 @@
 #include <stdlib.h>
-
 /**
- *
- *
- *
- *
- */
-
+ *str_concat - function that concatenate 2 strings using malloc
+ *@s1: first string
+ *@s2: second string
+ *Return: the pointer to the 2 strings concatenated */
 char *str_concat(char *s1, char *s2)
 {
 	char *z;
 	int i = 0, j = 0, k = 0, l = 0;
 
 	if (s1 == NULL)
-
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-
-	{
 		s2 = "";
-	}
-
 	while (s1[i] != '\0')
-
 	{
 		++i;
 	}
-
 	while (s2[j] != '\0')
-
 	{
 		++j;
 	}
-
-	z = malloc(sizeof(char) * (j + i) + 1);
-
+	z = malloc(sizeof(char) * (i + j) + 1);
 	if (z == NULL)
-
-	{
 		return (NULL);
-	}
-
 	while (k < i)
-
 	{
 		z[k] = s1[k];
 		k++;
 	}
-
 	while (l < j)
-
 	{
 		z[k] = s1[l];
 		k++;
 		l++;
 	}
-
-
 	return (z);
 }
