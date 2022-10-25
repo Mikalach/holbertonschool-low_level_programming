0b79635045e5d62dfd43d33935e86493ca73dc29
Mikalach <mika.djetic@gmaiul.com>
2022-10-25 06:30:29 -0700

    task 0 is easy

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function
+ * @b: int
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	     	exit(98);
+	(void)p;
+
+	return (p);
+}
