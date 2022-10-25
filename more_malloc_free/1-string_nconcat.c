a307d7a99ab78d552c3ca0246bc081878bb1347a
Mikalach <mika.djetic@gmaiul.com>
2022-10-25 09:38:29 -0700

    copying the exact code from last proj

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
 char *str_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *z;
-	int i = 0, j = 0, k = 0, l = 0;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
