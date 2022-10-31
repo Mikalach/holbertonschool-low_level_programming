727299bfc9bf071d5c9057cee5c04c9d65f9dbfe
Mikalach <mika.djetic@gmaiul.com>
2022-10-31 06:09:48 -0700

    hope you like my genocide

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @owner: dog owner
  */
 
-typedef struct dog
+typedef struct dog_T
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_T;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
