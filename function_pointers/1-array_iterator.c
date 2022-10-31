e4d3701d3f2b28f8f968312a9292c0e77e484dd8
Mikalach <mika.djetic@gmaiul.com>
2022-10-31 14:52:23 -0700

    dunno what i am doing

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,6 +12,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t n;
 
 	if (array && size && action)
-		for (n = 0, n < size, n++)
+		for (n = 0; n < size; n++)
 			action(array[n]);
 }
