6e635a55382af9f5cdfd0043d7693f3715ae18fa
Mikalach <mika.djetic@gmaiul.com>
2022-10-31 00:39:57 -0700

    header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+/**
+ * struct dog - create dog stuct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
