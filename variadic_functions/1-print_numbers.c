54a3c6c134f8327a0fd92dcfee20360fff077397
Mikalach <mika.djetic@gmaiul.com>
2022-11-01 02:52:54 -0700

    corrected the jump line conditions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,9 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list aptr;
 
 	if (n == 0)
+	{
 		printf("\n");
 		return;
-
+	}
 	va_start(aptr, n);
 
 	for (i = 0; i < n; i++)
