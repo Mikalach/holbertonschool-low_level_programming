e8babfce30ca6b82bd4b9dc851d0d01a28b1c070
Mikalach <mika.djetic@gmaiul.com>
2022-11-01 02:12:03 -0700

    removed the ";" after func

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,17 +6,24 @@
  *Return: 0 if n is null or the sum of the given numbers
  */
 
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
+
 {
+
 	unsigned int i;
 	int sum = 0;
 	va_list aptr;
 
 	if (n == 0)
 		return (0);
+
 	va_start (aptr, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(aptr, n);
+
 	va_end(aptr);
+
 	return (sum);
+
 }
