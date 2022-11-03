100d095ad88958e75550244f4b982d6d7c68ba2a
Mikalach <mika.djetic@gmaiul.com>
2022-11-03 02:32:52 -0700

    added a struct

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list vaptr);
+} print_type;
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
