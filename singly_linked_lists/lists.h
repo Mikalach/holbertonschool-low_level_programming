426a53ccd29114211c19676bbca4c2456fd766a8
Mikalach <mika.djetic@gmaiul.com>
2022-11-14 06:56:07 -0800

    added library

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
