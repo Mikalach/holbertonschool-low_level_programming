02cbaa6c189ee0856429844feb6c1d4e47f32113
Mikalach <mika.djetic@gmaiul.com>
2022-11-14 04:48:33 -0800

    added the comments

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ *list_len - function that return the number of element
+ *@Return: the number of elements
+ *@h: pointer to the nodes
+ */
+
+
 size_t list_len(const list_t *h)
 {
 	int i = 0;
