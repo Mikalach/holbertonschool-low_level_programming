282ea133173d8907b80a406ede96c1ec45ea7e43
Mikalach <mika.djetic@gmaiul.com>
2022-11-15 07:36:18 -0800

    trying by copying last proj

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - adds a node at the end
+ * @head: pointer of a list
+ * @str: pointer to a string
+ * Return: address
+ */
+list_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *sec;
+	listint_t *fir = *head;
+
+	sec = malloc(sizeof(listint_t));
+	if (!sec)
+		return (NULL);
+	sec->n = n;
+	sec->next = NULL;
+	if (!*head)
+	{
+		*head = sec;
+		return (*head);
+	}
+	while (fir->next)
+		fir = fir->next;
+	fir->next = sec;
+	return (fir);
+}
