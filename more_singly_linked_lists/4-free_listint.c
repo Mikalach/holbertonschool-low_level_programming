550307d6381dd15cdc4ae40303bd2e17fba8f726
Mikalach <mika.djetic@gmaiul.com>
2022-11-15 07:49:05 -0800

    copying last project

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that free the list created before
+ * @head: pointer to address of a list
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *clean;
+
+	while (head)
+	{
+		clean = head;
+		head = head->next;
+		free(clean->str);
+		free(clean);
+	}
+}
