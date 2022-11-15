e138f9a491ce45e412ec6de7c506c65982bf37ea
Mikalach <mika.djetic@gmaiul.com>
2022-11-15 04:46:22 -0800

    copied the last project

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add a node at the beginning of
+ * @head: pointer to address of a list
+ * @n: pointer to string
+ *
+ * Return: Address of new element
+ */
+list_t *add_nodeint(list_t **head, const int n)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
