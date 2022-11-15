5de027952b49b52d1a97983624b0778af52b7ec4
Mikalach <mika.djetic@gmaiul.com>
2022-11-15 04:20:14 -0800

    copying the first ex

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- *print_listint - print all the element of an int list
+ *listint_len - print all the element of an int list
  *@h: the pointer to the list
  *Return: the number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		printf("%d\n", i);
+		printf("%ld\n", i);
 		h = h->next;
 		++i;
 	}
