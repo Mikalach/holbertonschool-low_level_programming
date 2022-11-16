b696d2cffea0518ce16f9e319626890932248b30
Mikalach <mika.djetic@gmaiul.com>
2022-11-16 06:29:44 -0800

    change return in Return in the comment

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,7 @@
  *delete_nodeint_at_index - function that deletes a node at a given index
  *@head: pointer to address of a linked list
  *@index: index of the list where node should be deleted
- *return: 1 on success or 1 if no success
+ *Return: 1 on success or 1 if no success
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
