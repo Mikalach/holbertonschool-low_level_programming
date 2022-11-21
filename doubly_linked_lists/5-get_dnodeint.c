e668e3cb616a74f8a255c483d548a430dc85aefd
Mikalach <mika.djetic@gmaiul.com>
2022-11-21 10:16:02 -0800

    copeycopeycopey

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  * @index: index of the node
  * Return: nth node of a linked list or NULL if node doesnt exist
  */
-listint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
