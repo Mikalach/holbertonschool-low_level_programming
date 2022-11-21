1f5d5b8f1c2bb6a201683e9d943db0d11faa9e6b
Mikalach <mika.djetic@gmaiul.com>
2022-11-21 10:14:42 -0800

    copeycopeycopey

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns nth node of a linked list
+ * @head: pointer to the struct
+ * @index: index of the node
+ * Return: nth node of a linked list or NULL if node doesnt exist
+ */
+listint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		++i;
+	}
+	return (NULL);
+}
