b49fc8817c5622b5b97d9501bbda014f4d7f5e40
Mikalach <mika.djetic@gmaiul.com>
2022-11-22 01:47:58 -0800

    gab helped for the define

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	while ((n = read(fd1, buff, 1024)) > 0)
 		if ((write(fd2, buff, n)) != n || fd2 < 0)
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-				exit(0);
+				exit(99);
 	if (n < 0)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
