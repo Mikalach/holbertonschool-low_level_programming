b3a9afccec5e27c5408c69acd153f51b17585f2b
Mikalach <mika.djetic@gmaiul.com>
2022-11-25 02:08:59 -0800

    patch

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,7 +4,6 @@
  * add_dnodeint - add a node at the beginning of
  * @head: pointer to address of a list
  * @n: int in the node
- *
  * Return: Address of new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
@@ -19,5 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = *head;
 	*head = new;
+	if (*head)
+		(*head)->prev = new;
 	return (*head);
 }
