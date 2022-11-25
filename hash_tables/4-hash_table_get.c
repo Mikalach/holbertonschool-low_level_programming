e035b458c01c56fdc86f533d0808996c50b1751a
Mikalach <mika.djetic@gmaiul.com>
2022-11-25 01:55:10 -0800

    file is present

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_get - function that retrieves a value associated with a key
- * @ht: Hash table to look inside
+ * @ht: hash table to look inside
  * @key: key we want to retrieve
  * Return:the value associated with the element or NULL if key couldn’t be found
  */
@@ -17,7 +17,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		actual = ht->array[idx];
 		while (actual)
 		{
-			if (!strcmp(current->key, key))
+			if (!strcmp(actual->key, key))
 				return (actual->value);
 			actual = actual->next;
 		}
